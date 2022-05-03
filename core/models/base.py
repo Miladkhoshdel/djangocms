9c524aed6d9f09ecb53f7222588e6fa28e7a0810
Milad Khoshdel <miladkhoshdel@gmail.com>
2022-05-04 01:18:42 +0430

    Update Page Model

--- core/models/base.py
+++ core/models/base.py
@@ -3,9 +3,9 @@ from django.utils.translation import gettext_lazy as _
 
 
 class BaseModel(models.Model):
-    owner = models.CharField(max_length=200, name=_("Owner"))
-    create_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_add=True)
+    owner = models.CharField(_("Owner"), max_length=200)
+    create_date = models.DateTimeField(_('Created Date'), auto_now_add=True)
+    update_date = models.DateTimeField(_('Updated Date'), auto_now=True)
 
     class Meta:
         abstract = True
--- djangocms/settings.py
+++ djangocms/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+16dtke2m!ib!@qjnc$+yi&$z)s50y!toba41!0tj+5h9jov4)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1'] 
 
